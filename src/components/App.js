b03d04717dd78ce88d0a2f160704edf578585b3c
Alex Kiborgok <borgokalek14@gmail.com>
2022-05-04 13:07:54 +0300

    import components

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
+import PorkersList from "./PorkersList";
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<PorkersList porkers={hogs} />
 		</div>
 	);
 }
